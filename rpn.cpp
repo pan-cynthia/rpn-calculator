384848e612c65d0687857b40b4e988476001c5b7
Cynthia Pan <cynthiapan11@gmail.com>
2023-04-19 12:17:15 -0700

    implemented calculation for basic operations

--- rpn.cpp
+++ rpn.cpp
@@ -15,6 +15,10 @@ int main() {
 
   // parse given input by spaces
   vector<string> parsed = parse(input);
+
+  // calculate result
+  double result = rpn(parsed);
+  cout << result << endl;
 }
 
 vector<string> parse(string input) {
@@ -25,4 +29,60 @@ vector<string> parse(string input) {
     parsed.push_back(non_space);
   }
   return parsed;
+}
+
+double rpn(vector<string> input) {
+  // stack will store operands for use when an operator is encountered
+  vector<double> stack;
+
+  for (string s : input) {
+    if (isOperator(s)) {
+      //pop two operands off of the stack
+      double a = stack.back();
+      stack.pop_back();
+      double b = stack.back();
+      stack.pop_back();
+
+      // perform operation and push result back onto stack
+      stack.push_back(operation(a, b, s));
+    } else if (isOperand(s)) {
+      // element is an operand, push it onto the stack
+      stack.push_back(stod(s));
+    } else {
+      cout << "invalid input" << endl;
+      exit(1);
+    }
+  }
+
+  if (stack.size() > 1) {
+    cout << "error: incomplete postfix expression, missing operators/operands" << endl;
+    exit(1);
+  }
+
+  return stack.back();
+}
+
+bool isOperator(string op) {
+  return op == "+" || op == "-" || op == "*" || op == "/";
+}
+
+bool isOperand(string op) {
+  for (int i = 0; i < op.length(); ++i) {
+    // check if operand is a digit (0-9), a decimal symbol, or a negative symbol
+    if (!isdigit(op[i]) && op[i] != '.' && op[i] != '-') return false;
+  }
+  return true;
+}
+
+double operation(double a, double b, string op) {
+  if (op == "+") {
+    return b + a;
+  } else if (op == "-") { 
+    return b - a;
+  } else if (op == "*") {
+    return b * a;
+  } else if (op == "/") {
+    return b / a;
+  }
+  return -1;
 }
--- rpn.h
+++ rpn.h
@@ -9,4 +9,12 @@ using namespace std;
 // function that splits input string by spaces into an array
 vector<string> parse(string input);
 
+// function that calculates result of input expression
+double rpn(vector<string> input);
+
+bool isOperator(string op);
+bool isOperand(string op);
+
+double operation(double a, double b, string op);
+
 #endif // RPNCALCULATOR_RPN_H
