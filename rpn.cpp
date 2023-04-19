22c850b144fac3164195d8abb2b26f62d384737f
pan-cynthia <cypan@ucdavis.edu>
2023-04-19 11:13:34 -0700

    parse user input by space characters

--- rpn.cpp
+++ rpn.cpp
@@ -12,4 +12,14 @@ int main() {
   cout << "rpn> ";
   getline(cin, input);
 
+  // parse given input by spaces
+  vector<char> parsed = parse(input);
+}
+
+vector<char> parse(string input) {
+  vector<char> parsed;
+  for (char c : input) {
+    if (c != ' ') parsed.push_back(c);
+  }
+  return parsed;
 }
