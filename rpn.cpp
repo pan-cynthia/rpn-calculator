08ab6ec0e9d65b82b69ca83fbf8a86f251b8d729
Cynthia Pan <cynthiapan11@gmail.com>
2023-04-19 11:09:37 -0700

    add user input

--- rpn.cpp
+++ rpn.cpp
@@ -1,5 +1,15 @@
 #include "rpn.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int main() {
-  return 0;
+  // prompt user for input
+  string input;
+  cout << "rpn> ";
+  getline(cin, input);
+
 }
