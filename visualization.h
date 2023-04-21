33ee92bea3b6ae1492baefcac63faf044660bd6c
Cynthia Pan <cynthiapan11@gmail.com>
2023-04-20 19:39:11 -0700

    added way to print expression in parse tree format

---
+++ visualization.h
@@ -0,0 +1,12 @@
+#ifndef RPNCALCULATOR_VISUALIZATION_H
+#define RPNCALCULATOR_VISUALIZATION_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void list(vector<string> input);
+void printOutput(string output);
+
+#endif // RPNCALCULATOR_VISUALIZATION_H
