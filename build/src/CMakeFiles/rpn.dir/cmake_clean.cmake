337d6fb643fba4e0c8ab7cd8ed3a69b698ca9704
Cynthia Pan <cynthiapan11@gmail.com>
2023-05-16 11:42:11 -0700

    add build files

---
+++ build/src/CMakeFiles/rpn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpn.dir/rpn.cpp.o"
+  "CMakeFiles/rpn.dir/rpn.cpp.o.d"
+  "CMakeFiles/rpn.dir/visualization.cpp.o"
+  "CMakeFiles/rpn.dir/visualization.cpp.o.d"
+  "rpn"
+  "rpn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/rpn_lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpn_lib.dir/rpn.cpp.o"
+  "CMakeFiles/rpn_lib.dir/rpn.cpp.o.d"
+  "CMakeFiles/rpn_lib.dir/visualization.cpp.o"
+  "CMakeFiles/rpn_lib.dir/visualization.cpp.o.d"
+  "librpn_lib.dylib"
+  "librpn_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpn_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
